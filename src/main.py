68fde4b4cba032adbe2548e9b6cf78477bad9110
Gad Amr <gadelhaq.work@gmail.com>
2026-01-22 05:15:58 +0200

    feat: add MongoDB configuration and models; implement database connection in FastAPI app

--- src/main.py
+++ src/main.py
@@ -2,8 +2,21 @@ from fastapi import FastAPI
 from dotenv import load_dotenv
 load_dotenv()
 from routes import base,Data
+from motor.motor_asyncio import AsyncIOMotorClient
+from helpers.main_config.config import get_settings
 
 app = FastAPI()
+
+@app.on_event("startup")
+async def startup_db_client():
+    settings = get_settings()
+    app.mongo_conn = AsyncIOMotorClient(settings.MONGODB_URL)
+    app.db_client = app.mongo_conn[settings.MONGODB_DATABASE]
+
+
+@app.on_event("shutdown")
+async def shutdown_db_client():
+    app.mongo_conn.close()
 
 
 app.include_router(base.base_router)
